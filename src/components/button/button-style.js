f807e0431c91bac5abe488a3856c0c7d87c3ddf8
Verónica Isla <veronica.isla@kairosds.com>
2023-02-23 09:38:56 +0100

    feat: added styles to btn

--- src/components/button/button-style.js
+++ src/components/button/button-style.js
@@ -4,23 +4,23 @@ export const ButtonComponentStyle = css`
   .btn {
     padding: 5px 5px;
     min-width: 100px;
-    background-color: var(--color-positive);
-    border: 1px solid var(--color-grey-light-2);
-    border-radius: 1.5rem;
+    background-color: var(--color-white);
+    border: 2px solid var(--border-color);
+    border-radius: 8px;
     cursor: pointer;
-    font-size: var(--font-size-button);
-    line-height: var(--line-height-button);
     box-shadow: 0px 1px 2px rgba(166, 175, 195, 0.25);
+    font-size: 14px;
+    text-transform: uppercase;
   }
 
   .btn:hover {
-    background-color: var(--color-primary);
-    color: var(--color-positive);
+    background-color: var(--hover-color);
+    color: var(--color-white);
   }
 
   .disabled-btn {
     cursor: initial;
-    background-color: var(--color-grey-light-2);
+    background-color: var(--disabled-color);
     color: var(--color-grey);
   }
 
